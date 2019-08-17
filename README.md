cb22dd4b4cdc6de29b0281dd1533bc026fced0ca
rizikolik <ynsture@gmail.com>
2019-08-17 13:49:52 +0300

    Updates

--- README.md
+++
@@ -1,8 +0,0 @@
-# JS DRUM MACHİNE PROJECT WİTH REACT
-## This was the 3^rd Challange of the Free Code Camp's Front End Certification.
-#### With the help of the test button at the top left corner of the page you can see the Minumum Requirements of this Project.
-## What i used ?
-1. React
-2. Vanilla JS
-3. React Bootstrap
-### You can see the live version [here](https://rizikolik.github.io/JSDrumMachine/).
---
+++ precache-manifest.b6ce500b7840bf8dbef477aed947cb71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c6300c596a7b68de3d15cc8b401c3ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16c9e47f68d79564aa50",
+    "url": "/static/css/main.e30fa75f.chunk.css"
+  },
+  {
+    "revision": "39112b4cdad3c1417ed1",
+    "url": "/static/js/2.ae37b2dd.chunk.js"
+  },
+  {
+    "revision": "16c9e47f68d79564aa50",
+    "url": "/static/js/main.ce10276c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
